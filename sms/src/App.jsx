51b0972f8e0c9dd16e12444f50b57d89ed578429
dinu3509 <skarri@19gitam.in>
2025-01-26 21:54:38 +0530

    1-26-25-9.54pm

--- sms/src/App.jsx
+++ sms/src/App.jsx
@@ -1,13 +1,14 @@
 import { Link, Route, Routes, BrowserRouter } from "react-router-dom";
 import Login from "./pages/Login";
 import Alogin from "./pages/Alogin";
-
+import Shome from "./pages/Shome";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/alogin" element={<Alogin />} />
+        <Route path="/home" element={<Shome />} />
       </Routes>
     </BrowserRouter>
   );
--- sms/src/assets/index.js
+++ sms/src/assets/index.js
@@ -1,5 +1,6 @@
 import bg from '../assets/bg.jpg'
 import mbg from '../assets/mbg.jpg'
+import zoro from '../assets/zoro.jpg'
 
 
-export  {bg,mbg};
+export  {bg,mbg,zoro};
