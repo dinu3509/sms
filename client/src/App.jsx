7b35a57dd2833f318bb87321b83b53ffe7c95e79
dinu3509 <skarri@19gitam.in>
2025-02-03 22:45:52 +0530

    2.3.25.4.22

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path="https://school-1rzs.vercel.app/login" element={<Login />} />
         <Route path="/alogin" element={<Alogin />} />
         <Route path="/home" element={<Shome />} />
       </Routes>
